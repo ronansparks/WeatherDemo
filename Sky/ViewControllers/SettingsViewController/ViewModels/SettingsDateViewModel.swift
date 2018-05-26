d3f154d52685e1491fca9e33cc0f897edbecadde
ronansparks <ronansparks@gmail.com>
2018-05-26 11:40:12 +0800

    change setting vc to mvvm

---
+++ Sky/ViewControllers/SettingsViewController/ViewModels/SettingsDateViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsDateViewModel.swift
+//  Sky
+//
+//  Created by Ronan on 5/26/18.
+//  Copyright © 2018 Mars. All rights reserved.
+//
+
+import UIKit
+
+struct SettingsDateViewModel {
+    let dateMode: DateMode
+    
+    var labelText: String {
+        return dateMode == .text ? "Fri, 01 December" : "F, 12/01"
+    }
+    
+    var accessory: UITableViewCellAccessoryType {
+        if UserDefaults.dateMode() == dateMode {
+            return .checkmark
+        }
+        else {
+            return .none
+        }
+    }
+}
---
+++ Sky/ViewControllers/SettingsViewController/ViewModels/SettingsTemperatureViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsTemperatureViewModel.swift
+//  Sky
+//
+//  Created by Ronan on 5/26/18.
+//  Copyright © 2018 Mars. All rights reserved.
+//
+
+import UIKit
+
+struct SettingsTemperatureViewModel {
+    let temperatureMode: TemperatureMode
+    
+    var labelText: String {
+        return temperatureMode == .celcius ? "Celcius" : "Fahrenheit"
+    }
+    
+    var accessory: UITableViewCellAccessoryType {
+        if UserDefaults.temperatureMode() == temperatureMode {
+            return .checkmark
+        }
+        else {
+            return .none
+        }
+    }
+}
